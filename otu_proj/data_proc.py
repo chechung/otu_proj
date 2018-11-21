7c15316413e7cfc52b96f107b078f6ea396ab7ed
Carolina Chung <chechung@umich.edu>
2018-11-21 14:43:37 -0500

    Updated all files in otu_proj into final version to be pushed into GitHub.

--- otu_proj/data_proc.py
+++ otu_proj/data_proc.py
@@ -3,26 +3,35 @@
 
 """
 data_proc.py
-Process OTU data to generate phylogenetic abundance bar plot
+Process OTU data to generate phylogenetic abundance group bar plot
 
 General steps:
     1. separate values based on control/treatment category
     2. calculate mean and standard deviation values for phylogenetic groups per control/treatment group
-    3. generate bar plot comparing average abundance values between control/treatment groups
+    3. generate group bar plot comparing average abundance values between control/treatment groups
        for each phylogenetic group
+
+Outputs:
+    - filename_processed_.xlsx: excel file processed data with the following sheets:
+        1. data: relative abundance values for phylogenetic groups that were detected in at least one sample
+        2. control: subset of data for control group, as well as the mean and standard deviation values
+        3. low: subset of data for low treatment group, as well as the mean and standard deviation values
+        4. medium: subset of data for medium treatment group, as well as the mean and standard deviation values
+        5. high: subset of data for high treatment group, as well as the mean and standard deviation values
+    - filename.png: image file that compares relative abundance between control treatment groups for each
+                    phylogenetic group
 """
 
 import sys
 import argparse
 import pandas as pd
-from openpyxl import load_workbook
 import matplotlib.pyplot as plt
 import os
 
 SUCCESS = 0
 IO_ERROR = 2
 
-DEF_DATA_FILE = 'data.xlsx'
+DEF_DATA_FILE = 'data/OTU_data.xlsx'
 DEF_FIG_DIM = (20, 10)
 DEF_FONT_SIZE = 15
 DEF_GRID_BOOL = True
@@ -33,30 +42,34 @@ def warning(*objs):
     print("WARNING: ", *objs, file=sys.stderr)
 
 
-def data_process_analysis(data_frame, file_path, data_fname):
+def data_process_analysis(data_frame, fname):
     """
-    Two main functions:
-    1. Separate data into control or treatment groups and add subset data into excel file
-    2. Attain mean and standard deviation values for each phylogenetic group in each group
+    Processing steps:
+    1. Remove phylogenetic groups absent in all samples
+    2. Separate data into control or treatment groups
+    3. Attain mean and standard deviation values for each phylogenetic group in each group
+    4. Transfer processed data with mean and standard deviation values onto output excel file
 
     Parameters
     ----------
-    data_frame: pandas dataframe of OTU values
+    data_frame: pandas dataframe of OTU file
+    fname: name for output xlsx file (taken from input data)
 
     Returns
     -------
-    means_df: dataframe of mean values where row index corresponds to control/treat group and
+    means_df: dataframe of mean values where row index corresponds to control/treatment group and
               column index corresponds to phylogenetic group
-    sd_df: dataframe of standard deviation values where row index corresponds to control/treat group and
+    sd_df: dataframe of standard deviation values where row index corresponds to control/treatment group and
            column index corresponds to phylogenetic group
     """
 
+    data_frame = data_frame.loc[:, (data_frame != 0).any(axis=0)]
     control_vals = []
     low_vals = []
     med_vals = []
     high_vals = []
-    for i in range(0,data_frame.shape[0]):
-        for j in range(0,data_frame.shape[1]):
+    for i in range(0, data_frame.shape[0]):
+        for j in range(0, data_frame.shape[1]):
             if 'control' in data_frame.index[i]:
                 control_vals += [data_frame.index[i]]
             elif 'low' in data_frame.index[i] and 'treated' in data_frame.index[i]:
@@ -69,8 +82,9 @@ def data_process_analysis(data_frame, file_path, data_fname):
     groups_list = list(groups)
 
     # calculate average and SD values for each group and add to excel file as individual worksheets
-    writer = pd.ExcelWriter(file_path, engine='openpyxl')
-    writer.book = load_workbook(file_path)
+    output = fname + '_processed.xlsx'
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    data_frame.to_excel(writer, sheet_name='data')
     means_df = pd.DataFrame(columns=list(data_frame))
     sd_df = pd.DataFrame(columns=list(data_frame))
     for i in range(0, len(groups)):
@@ -82,20 +96,18 @@ def data_process_analysis(data_frame, file_path, data_fname):
         means_df.loc['{}'.format(label)] = x.loc['Mean']
         sd_df.loc['{}'.format(label)] = x.loc['SD']
     writer.save()
-    print("Updated file: {}".format(data_fname))
+    print("Wrote file: {}".format(output))
     return means_df, sd_df
 
 
 def plot_data(mean_values, sd_values, fig_dim, font_size, grid_bool, fname):
     """
-    Plot group bar plot showing mean relative abundance of each phylogenetic group with error bars
+    Generate group bar plot showing mean relative abundance of each phylogenetic group with error bars
 
     Parameters
     ----------
-    mean_values: dataframe of mean values where row index corresponds to control/treat group and
-                 column index corresponds to phylogenetic group
-    sd_values: dataframe of standard deviation values where row index corresponds to control/treat group and
-           column index corresponds to phylogenetic group
+    mean_values: dataframe of mean values generated from data_process_analysis
+    sd_values: dataframe of standard deviation values generated from data_process_analysis
     fig_dim: tuple of integer values specifying figure dimension; (horizontal_length, vertical_length)
              (DEFAULT=(20, 10))
     font_size: integer value specifying font_size for axis, tick, and legend labels (DEFAULT=15)
@@ -104,11 +116,12 @@ def plot_data(mean_values, sd_values, fig_dim, font_size, grid_bool, fname):
 
     Returns
     -------
-    saves a png file of bar plot
+    saves a png file of group bar plot
     """
+    output = fname + '.png'
     mean_values = mean_values.T
     ax = mean_values.plot.bar(figsize=fig_dim, width=0.5, yerr=sd_values.T)
-    ax.set_xlabel("Phylum", fontsize=font_size)
+    ax.set_xlabel("Phylogenetic Group", fontsize=font_size)
     ax.set_ylabel("Relative Abundance", fontsize=font_size)
     ax.set_xticklabels(mean_values.index)
     plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
@@ -116,8 +129,8 @@ def plot_data(mean_values, sd_values, fig_dim, font_size, grid_bool, fname):
     plt.yticks(size=font_size)
     plt.legend(prop={'size': font_size})
     plt.grid(grid_bool)
-    plt.savefig(fname + '.png', bbox_inches="tight")
-    print("Wrote file: {}".format(fname + '.png'))
+    plt.savefig(output, bbox_inches="tight")
+    print("Wrote file: {}".format(output))
 
 
 def parse_cmdline(argv):
@@ -132,7 +145,7 @@ def parse_cmdline(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--xlsx_data_file", help="The location of xlsx file of OTU data.",
                         default=DEF_DATA_FILE)
-    parser.add_argument("-f", "--fig_dim", help="Figure dimension for bar plot (integer tuple).",
+    parser.add_argument("-f", "--fig_dim", help="Figure dimension for group bar plot (integer tuple).",
                         default=DEF_FIG_DIM)
     parser.add_argument("-s", "--font_size", help="Font size for axis, tick, and legend labels (integer value).",
                         default=DEF_FONT_SIZE)
@@ -156,8 +169,7 @@ def main(argv=None):
         return ret
     data_fname = os.path.basename(args.xlsx_data_file)
     fname = os.path.splitext(data_fname)[0]
-    means_df, sd_df = data_process_analysis(args.xlsx_data, args.xlsx_data_file, data_fname)
-
+    means_df, sd_df = data_process_analysis(args.xlsx_data, fname)
     plot_data(means_df, sd_df, args.fig_dim, args.font_size, args.grid_bool, fname)
     return SUCCESS  # success
 
